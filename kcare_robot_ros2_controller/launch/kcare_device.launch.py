d3379f91e0a3fcbd7d03fd0a57c64012abb59199
ferglie0812 <ferglie@naver.com>
2025-07-11 15:29:41 +0900

    azure camera setup dev branch

--- kcare_robot_ros2_controller/launch/kcare_device.launch.py
+++ kcare_robot_ros2_controller/launch/kcare_device.launch.py
@@ -26,25 +26,25 @@ def generate_launch_description():
             }]
         ),
         
-        # Gripper Control Node
-        Node(
-            package='kcare_robot_ros2_controller',
-            executable='gripper_control_node',
-            name='gripper_control_node',
-            output='screen',
-            parameters=[{
-                # 필요한 경우 파라미터를 추가하세요
-            }]
-        ),
+        # # Gripper Control Node
+        # Node(
+        #     package='kcare_robot_ros2_controller',
+        #     executable='gripper_control_node',
+        #     name='gripper_control_node',
+        #     output='screen',
+        #     parameters=[{
+        #         # 필요한 경우 파라미터를 추가하세요
+        #     }]
+        # ),
 
-        # Device Server Node
-        Node(
-            package='kcare_robot_ros2_controller',
-            executable='device_node',
-            name='device_node',
-            output='screen',
-            parameters=[{
-                # 필요한 경우 파라미터를 추가하세요
-            }]
-        ),
+        # # Device Server Node
+        # Node(
+        #     package='kcare_robot_ros2_controller',
+        #     executable='device_node',
+        #     name='device_node',
+        #     output='screen',
+        #     parameters=[{
+        #         # 필요한 경우 파라미터를 추가하세요
+        #     }]
+        # ),
     ])
